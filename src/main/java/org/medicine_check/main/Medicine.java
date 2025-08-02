7014f0183e6699b2e12775455b1cb75b6cb554a8
nubah970 <nubah100@gmail.com>
2025-08-02 19:17:44 -0400

    created the csv

---
+++ src/main/java/org/medicine_check/main/Medicine.java
@@ -0,0 +1,15 @@
+package org.medicine_check.main;
+public class Medicine {
+   private  String name;
+    private String dose;
+    private String time;
+    public Medicine(String name, String dose, String time) {
+        this.name = name;
+        this.dose = dose;
+        this.time = time;
+    }
+    public String getName() { return name; }
+    public String getDose() { return dose; }
+    public String getTime() { return time; }
+} 
+}
